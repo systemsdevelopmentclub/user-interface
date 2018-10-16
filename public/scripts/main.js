d09392f571a57ff2b57e2562390e15fadb486f23
Jacob Hemmerich <jhem@iastate.edu>
2018-10-16 12:50:04 -0500

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,20 @@
-    //This program is free software: you can redistribute it and/or modify
-    //it under the terms of the GNU General Public License as published by
-    //the Free Software Foundation, either version 3 of the License, or
-    //(at your option) any later version.
-
-    //This program is distributed in the hope that it will be useful,
-    //but WITHOUT ANY WARRANTY; without even the implied warranty of
-    //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    //GNU General Public License for more details.
-
-    //You should have received a copy of the GNU General Public License
-    //along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/*
+ * @source: https://github.com/systemsdevelopmentclub/user-interface
+ * @license: https://www.gnu.org/licenses/lgpl.txt
+ * Copyright (c) 2018 Systems Development Club
+ * This file is part of SDC Search.
+ * SDC Search is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * SDC Search is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with SDC Search.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 //This script will take queries from main.html and send them to results-ranking
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, world.';
+var query = document.getElementById('query').value
+
